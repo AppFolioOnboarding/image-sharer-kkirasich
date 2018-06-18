ffe6c22379658fe264f6a5bba9a0fb21db9b5b0f
boying.shi <shi.boying@gmail.com>
2018-06-17 21:03:34 -0700

    bs - add api endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#home'
+
+  namespace :api do
+    resource :feedbacks, only: [:create]
+  end
 end
