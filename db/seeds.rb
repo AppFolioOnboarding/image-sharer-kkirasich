936497ad8ae8da39a4a4d2755daa5fc1173e6cec
Kaitlin Kirasich <kaitlin.kirasich@appfolio.com>
2019-03-20 14:17:29 -0700

    kk - Add seed images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,62 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+dogs = [{
+  url: 'https://images.pexels.com/photos/356378/pexels-photo-356378.jpeg'
+},
+        {
+          url: 'https://images.pexels.com/photos/39317/chihuahua-dog-puppy-cute-39317.jpeg'
+        },
+        {
+          url: 'https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg'
+        },
+        {
+          url: 'https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg'
+        },
+        {
+          url: 'https://images.pexels.com/photos/1345191/pexels-photo-1345191.jpeg'
+        },
+        {
+          url: 'https://images.pexels.com/photos/1805164/pexels-photo-1805164.jpeg'
+        },
+        {
+          url: 'https://images.pexels.com/photos/1851164/pexels-photo-1851164.jpeg'
+        },
+        {
+          url: 'https://images.pexels.com/photos/8700/wall-animal-dog-pet.jpg'
+        },
+        {
+          url: 'https://images.pexels.com/photos/1663421/pexels-photo-1663421.jpeg'
+        },
+        {
+          url: 'https://images.pexels.com/photos/1254140/pexels-photo-1254140.jpeg'
+        },
+        {
+          url: 'https://images.pexels.com/photos/33053/dog-young-dog-small-dog-maltese.jpg'
+        },
+        {
+          url: 'https://images.pexels.com/photos/1458925/pexels-photo-1458925.jpeg'
+        },
+        {
+          url: 'https://images.pexels.com/photos/1322182/pexels-photo-1322182.jpeg'
+        },
+        {
+          url: 'https://images.pexels.com/photos/248307/pexels-photo-248307.jpeg'
+        },
+        {
+          url: 'https://images.pexels.com/photos/485294/pexels-photo-485294.jpeg'
+        },
+        {
+          url: 'https://images.pexels.com/photos/59523/pexels-photo-59523.jpeg'
+        },
+        {
+          url: 'https://images.pexels.com/photos/731022/pexels-photo-731022.jpeg'
+        },
+        {
+          url: 'https://images.pexels.com/photos/374898/pexels-photo-374898.jpeg'
+        },
+        {
+          url: 'https://images.pexels.com/photos/434090/pexels-photo-434090.jpeg'
+        },
+        {
+          url: 'https://images.pexels.com/photos/460823/pexels-photo-460823.jpeg'
+        }]
+
+Image.create!(dogs)
