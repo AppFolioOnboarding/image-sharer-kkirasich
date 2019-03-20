22223d4727ceae4b8ecff563babe2f0c94f2aade
Kaitlin Kirasich <kaitlin.kirasich@appfolio.com>
2019-03-20 15:07:05 -0700

    kk - Add image tags

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,6 @@
 class Image < ApplicationRecord
   validate :must_be_valid_url
+  acts_as_taggable
 
   def must_be_valid_url
     errors.add(:url, 'must be a valid URL') unless URI.parse(url).is_a?(URI::HTTP) ||
