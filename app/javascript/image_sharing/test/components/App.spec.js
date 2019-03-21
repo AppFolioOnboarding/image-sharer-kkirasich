269f08e68fc765fb290cc9e5e9d4a71eea6618a0
Kaitlin Kirasich <kaitlin.kirasich@appfolio.com>
2019-03-21 10:55:44 -0700

    kk - Add form

--- app/javascript/image_sharing/test/components/App.spec.js
+++ app/javascript/image_sharing/test/components/App.spec.js
@@ -10,4 +10,18 @@ describe('<App />', () => {
     const wrapper = mount(<App />);
     assert(wrapper.contains('Tell us what you think'));
   });
+
+  it('should render a form', () => {
+    const wrapper = mount(<App />);
+    const form = wrapper.find('Form');
+    assert.strictEqual(form.length, 1);
+    assert.strictEqual(form.find('Input').length, 2);
+  });
+
+  it('should render a footer', () => {
+    const wrapper = mount(<App />);
+    const footer = wrapper.find('footer');
+    assert.strictEqual(footer.length, 1);
+    assert.strictEqual(footer.text(), 'Copyright: Appfolio Inc. Onboarding');
+  });
 });
